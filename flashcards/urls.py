a1e11a46357457c680787e266095163e04a5db0f
jim-a-jenkins <jimajenkins87@gmail.com>
2023-01-24 20:57:54 -0700

    finish basic api endpoints

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from flashcards.views import Flashcards, Game
 from flashcards.views import QuestionsListAPIView
 from flashcards.views import QuestionRetrieveAPIView
-from flashcards.views import ScoresListAPIView
+from flashcards.views import ScoresListAPIView, ScoreDestroyAPIView
 from flashcards.views import SessionsListCreateAPIView
 from flashcards.views import SessionRetrieveUpdateDestroyAPIView
 
@@ -16,6 +16,7 @@ urlpatterns = [
     path("api/v1/questions/", QuestionsListAPIView.as_view(), name="questions"),
     path("api/v1/questions/<int:pk>", QuestionRetrieveAPIView.as_view(), name="question"),
     path("api/v1/scores/", ScoresListAPIView.as_view(), name="scores"),
+    path("api/v1/scores/<int:pk>", ScoreDestroyAPIView.as_view(), name="score_destroy"),
     path("api/v1/sessions/", SessionsListCreateAPIView.as_view(), name="sessions"),
     path("api/v1/sessions/<uuid:pk>", SessionRetrieveUpdateDestroyAPIView.as_view(), name="session"),
 ]
--- flashcards/views.py
+++ flashcards/views.py
@@ -10,6 +10,9 @@ from flashcards.serializers import SessionSerializer
 from flashcards.serializers import QuestionSerializer
 from rest_framework import generics, permissions
 from typing import Dict, List, Union
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.exceptions import APIException
 
 import random
 
@@ -167,6 +170,9 @@ def create_session(request: HttpRequest, cd: Dict[str, Union[bool, int]]):
     session = Session.objects.create(
         include_trees=cd["include_trees"],
         include_shrubs=cd["include_shrubs"],
+        include_herbs=cd["include_herbs"],
+        include_lichens=cd["include_lichens"],
+        include_poisonous=cd["include_poisonous"],
         include_colorado=cd["include_colorado"],
         include_idaho=cd["include_idaho"],
         include_montana=cd["include_montana"],
@@ -180,36 +186,36 @@ def create_session(request: HttpRequest, cd: Dict[str, Union[bool, int]]):
     return session
 
 
-def get_excluded_groups(cd: Dict[str, Union[bool, int]]) -> List[str]:
+def get_excluded_groups(data: Dict[str, Union[bool, int]]) -> List[str]:
     exclude_groups: List[str] = []
-    if cd["include_trees"] is False:
+    if data["include_trees"] is False:
         exclude_groups.append('Trees')
-    if cd["include_shrubs"] is False:
+    if data["include_shrubs"] is False:
         exclude_groups.append('Shrubs')
-    if cd["include_herbs"] is False:
+    if data["include_herbs"] is False:
         exclude_groups.append('Herbs')
-    if cd["include_lichens"] is False:
+    if data["include_lichens"] is False:
         exclude_groups.append('Lichens')
-    if cd["include_poisonous"] is False:
+    if data["include_poisonous"] is False:
         exclude_groups.append('Poisonous')
     return exclude_groups
 
 
-def get_excluded_regions(cd: Dict[str, Union[bool, int]]) -> Dict[str, bool]:
+def get_excluded_regions(data: Dict[str, Union[bool, int]]) -> Dict[str, bool]:
     region_kwargs: Dict[str, bool] = {}
-    if cd["include_colorado"]:
+    if data["include_colorado"]:
         region_kwargs["in_colorado"] = True
-    if cd["include_idaho"]:
+    if data["include_idaho"]:
         region_kwargs["in_idaho"] = True
-    if cd["include_montana"]:
+    if data["include_montana"]:
         region_kwargs["in_montana"] = True
-    if cd["include_new_mexico"]:
+    if data["include_new_mexico"]:
         region_kwargs["in_new_mexico"] = True
-    if cd["include_utah"]:
+    if data["include_utah"]:
         region_kwargs["in_utah"] = True
-    if cd["include_washington"]:
+    if data["include_washington"]:
         region_kwargs["in_washington"] = True
-    if cd["include_wyoming"]:
+    if data["include_wyoming"]:
         region_kwargs["in_wyoming"] = True
     return region_kwargs
 
@@ -231,6 +237,76 @@ def get_extra_options_names(plants: QuerySet[Plant]):
     extra_options_names = [option.name for option in extra_options]
     return extra_options_names
 
+# API
+
+def generate_questions_api(data: Dict[str, Union[bool, int]], session_id) -> None:
+    counter = 0
+    exclude_groups = get_excluded_groups(data)
+    region_kwargs = get_excluded_regions(data)#TODO update - function doesn't do what it says it does
+    plants = Plant.objects.filter(Q(**region_kwargs, _connector=Q.OR))
+    for group in exclude_groups:
+        plants = plants.exclude(group__exact=group)
+    extra_options_names = None
+    if len(plants) < 4:
+        extra_options_names = get_extra_options_names(plants)
+    plant_names = [plant.name for plant in plants]
+    while counter < data["total_questions"]:
+        random.shuffle(plant_names)
+        for plant in plant_names:
+            if counter == data["total_questions"]:
+                break
+            images = Image.objects.filter(plant__name__exact=plant)
+            image = images[random.randint(0, len(images) - 1)]
+            image.__setattr__("image_name", image.__str__())
+            answer = plant
+            choices = {"a": None, "b": None, "c": None, "d": None}
+            answer_choice = random.choice(list(choices))
+            choices[answer_choice] = answer
+            other_options = plant_names.copy()
+            if extra_options_names:
+                other_options.extend(extra_options_names)
+                other_options = [*set(other_options)] # remove duplicates
+            for key, _ in choices.items():
+                if choices[key] is None:
+                    try_again = True
+                    while try_again is True:
+                        choice = random.choice(other_options)
+                        if choice != answer and choice in other_options:
+                            choices[key] = choice
+                            other_options.remove(choice)
+                            try_again = False
+            Question.objects.create(
+                session_id=session_id,
+                plant_image=image,
+                question_name=str(session_id) + f"_{counter}",
+                choice_a=choices["a"],
+                choice_b=choices["b"],
+                choice_c=choices["c"],
+                choice_d=choices["d"],
+                answer=answer,
+            )
+            counter += 1
+
+
+def create_session_api(user_id: int, data: Dict[str, Union[bool, int]]):
+    session = Session.objects.create(
+        include_trees=data["include_trees"],
+        include_shrubs=data["include_shrubs"],
+        include_herbs=data["include_herbs"],
+        include_lichens=data["include_lichens"],
+        include_poisonous=data["include_poisonous"],
+        include_colorado=data["include_colorado"],
+        include_idaho=data["include_idaho"],
+        include_montana=data["include_montana"],
+        include_new_mexico=data["include_new_mexico"],
+        include_utah=data["include_utah"],
+        include_washington=data["include_washington"],
+        include_wyoming=data["include_wyoming"],
+        total_questions=data["total_questions"],
+        session_user_id=user_id,
+    )
+    return session
+
 class QuestionsListAPIView(generics.ListAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
@@ -248,14 +324,49 @@ class ScoresListAPIView(generics.ListAPIView):
     serializer_class = ScoreSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+class ScoreDestroyAPIView(generics.DestroyAPIView):
+    queryset = Score.objects.all()
+    serializer_class = ScoreSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
 
 class SessionsListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = SessionSerializer
     permission_classes = [permissions.IsAuthenticated]
     
+    class SessionExistsException(APIException):
+        status_code = 400
+        default_detail = 'A session already exists for this user.'
+    
     def get_queryset(self):
         user = self.request.user
         return Session.objects.filter(session_user=user)
+    
+    def perform_create(self, serializer):
+        session = Session.objects.filter(session_user_id=self.request.user.id).first()
+        if session:
+            raise self.SessionExistsException
+        else:
+            if serializer.is_valid():
+                session = serializer.save(
+                    session_user=self.request.user,
+                    include_trees=self.request.data.get("include_trees"),
+                    include_shrubs=self.request.data.get("include_shrubs"),
+                    include_herbs=self.request.data.get("include_herbs"),
+                    include_lichens=self.request.data.get("include_lichens"),
+                    include_poisonous=self.request.data.get("include_poisonous"),
+                    include_colorado=self.request.data.get("include_colorado"),
+                    include_idaho=self.request.data.get("include_idaho"),
+                    include_montana=self.request.data.get("include_montana"),
+                    include_new_mexico=self.request.data.get("include_new_mexico"),
+                    include_utah=self.request.data.get("include_utah"),
+                    include_washington=self.request.data.get("include_washington"),
+                    include_wyoming=self.request.data.get("include_wyoming"),
+                    total_questions=self.request.data.get("total_questions")
+                )
+                generate_questions_api(self.request.data, session.session_id)
+                return Response(session.session_id, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class SessionRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SessionSerializer
